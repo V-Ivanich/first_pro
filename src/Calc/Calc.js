55a450fd97736b392de23e0ebcb07026ad2909f9
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-05-25 14:48:12 +0300

    by lesson

--- src/Calc/Calc.js
+++ src/Calc/Calc.js
@@ -7,31 +7,25 @@ class Calc extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'name': 150,
       'result': 0,
-      'value':'EUR'
     }
   }
 
-  seletCurrency = (e) => {
-    this.setState({value: e.target.value})
+  static getDerivedStateFromProps(props, state) {
+    return {rate: props.rate};
   }
 
   calcRate = (event) => {
     event.preventDefault();
     let elements = event.target.elements;
+    console.log(elements);
     console.log(elements['type-currency'].value);
-    let vl = this.state.value;
-    let koff = this.props.rate[vl];
-    let koll = this.state.name;
-    this.setState({result: (koll / koff).toFixed(3)});
+    console.log(elements['count-currency'].value);
+    let tCurr = elements['type-currency'].value;// валюта
+    let cCurr = elements['count-currency'].value;// количество
+    this.setState({result: (cCurr / this.state.rate[tCurr]).toFixed(3)})
   }
 
-  change = (e) => {
-    let val = e.target.value;
-    this.setState({name: val});
-    console.log(this.state.name);
-  }
 
   render() {
   return (
@@ -43,9 +37,9 @@ class Calc extends React.Component {
             <div>
               <form onSubmit={this.calcRate}>
 
-              <input type='number' defaultValue={this.state.name} onChange={this.change} />
-              <select name='type-currency' value={this.state.value} onChange={this.seletCurrency}>
-                  {Object.keys(this.props.rate).map((keyName, i) =>
+              <input type='number' defaultValue='150' name='count-currency'/>
+              <select name='type-currency' >
+                  {Object.keys(this.state.rate).map((keyName, i) =>
                     (
                       <option key={keyName} value={keyName}>{keyName}</option>
                     )
